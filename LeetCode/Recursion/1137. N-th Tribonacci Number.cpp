fe791f497d6cb7fd08bae8bca7e159be2db6fcef
mohitmishra786 <immadmohit@gmail.com>
2023-08-20 14:02:17 +0530

    Added Memoization Solution for 1137. Nth Tribonacci Number

--- LeetCode/Recursion/1137. N-th Tribonacci Number.cpp
+++ LeetCode/Recursion/1137. N-th Tribonacci Number.cpp
@@ -16,3 +16,29 @@ public:
         return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
     }
 };
+
+
+// DP with Memoization
+
+class Solution {
+public:
+    int dp[40];
+    int solve(int n){
+        if(n <= 0){
+            return dp[n] = 0;
+        }
+        if(n == 1 or n == 2){
+            return dp[n] = 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = solve(n - 1) + solve(n - 2) + solve(n - 3);
+    }
+    int tribonacci(int n) {
+        memset(dp, -1, sizeof(dp));
+        int ans = solve(n);
+        return ans;
+    }
+};
