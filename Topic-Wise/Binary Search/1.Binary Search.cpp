766abbae043bbeae7cd1aff2c6b682d697c93686
mohitmishra786 <immadmohit@gmail.com>
2024-01-21 14:44:21 +0530

    Added Solution

--- Topic-Wise/Binary Search/1.Binary Search.cpp
+++ Topic-Wise/Binary Search/1.Binary Search.cpp
@@ -15,3 +15,23 @@ int search(vector<int> &nums, int target) {
     }
     return -1;
 }
+
+// Recursive Solution
+
+int solve(vector<int> &nums, int target, int l, int r){
+    if(r < l){
+        return -1;
+    }
+    int mid = (l + r)/2;
+    if(target == nums[mid]) return mid;
+    else if(target > nums[mid]) solve(nums, target, mid + 1, r);
+    else solve(nums, target, l, mid - 1);
+}
+
+int search(vector<int> &nums, int target) {
+    // Write your code here.
+    int n = nums.size();
+    int l = 0;
+    int r = n - 1;
+    return solve(nums, target, l, r);
+}
