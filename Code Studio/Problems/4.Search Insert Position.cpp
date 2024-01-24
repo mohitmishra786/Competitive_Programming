0bd7430c1497bdce6c3e564b8a090abaa86fc64a
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2024-01-24 11:18:52 +0530

    Create 4.Search Insert Position.cpp

---
+++ Code Studio/Problems/4.Search Insert Position.cpp
@@ -0,0 +1,19 @@
+// It will be same as Lower Bound
+// Question Link:- https://bit.ly/3ocI0HW 
+
+int searchInsert(vector<int>& arr, int m)
+{
+	// Write your code here.
+	int n = arr.size();
+	int low =0, high = n - 1;
+	int ans = n;
+	while(low <= high){
+		int mid = low + (high - low)/2;
+		if(arr[mid] >= m){
+			ans = mid;
+			high = mid - 1;
+		}
+		else low = mid + 1;
+	}
+	return ans;
+}
