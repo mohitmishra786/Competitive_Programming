9119f771abf7caba3c30b783479ca1e761bf7e96
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2024-01-24 14:46:46 +0530

    Create 5.Ceil The Floor.cpp

---
+++ Topic-Wise/Binary Search/5.Ceil The Floor.cpp
@@ -0,0 +1,33 @@
+// https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbm9kZDlVMUhxWVhhZU01bEFQOU5ZNWFSZmNmZ3xBQ3Jtc0tubnViN0UwUUhibGtUYUVkc3llT3JDM3g0SjBfSnBxWS1QQ191cllsTWZjeWV1elc3cVhZemRpQjJfeE8tQjEwUDJqQlFLTGRqR0Rfdmw4b203UUhXUkFDZTllS3c3STY2QWswMlRpSmJYa3Zra2lEdw&q=https%3A%2F%2Fwww.codingninjas.com%2Fcodestudio%2Fproblems%2Fceiling-in-a-sorted-array_1825401&v=6zhGS79oQ4k
+
+pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
+	// Write your code here.
+	
+	// Ceil (Lower Bound Only)
+	int low = 0, high = n - 1;
+	int lb = -1;
+	while(low <= high){
+		int mid = (high + low)/2;
+		if(a[mid] >= x){
+			lb = a[mid];
+			high = mid - 1;
+		}
+		else low = mid + 1;
+	}
+
+
+	// Floor
+	low = 0, high = n - 1;
+	int ans = -1;
+	while(low <= high){
+		int mid = low + (high - low)/2;
+		if(a[mid] <= x){
+			ans = a[mid];
+			low = mid + 1;
+		}
+		else{
+			high = mid - 1;
+		}
+	}
+	return {ans, lb};
+}
