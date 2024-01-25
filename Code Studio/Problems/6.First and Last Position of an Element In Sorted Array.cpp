72370e8502d2829aa9a26de5e7941b46a9587d4d
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2024-01-25 12:23:57 +0530

    Update 6.First and Last Position of an Element In Sorted Array.cpp

--- Code Studio/Problems/6.First and Last Position of an Element In Sorted Array.cpp
+++ Code Studio/Problems/6.First and Last Position of an Element In Sorted Array.cpp
@@ -1,3 +1,37 @@
+//  Question Link:- https://bit.ly/3Mdw2FX
+
+// LB and UB Solution
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int x)
+{
+    // Write your code here
+    int low = 0, high = n - 1;
+    int lb = n;
+    while(low <= high){
+        int mid = (high + low)/2;
+        if(arr[mid] >= x){
+            lb = mid;
+            high = mid - 1;
+        }
+        else low = mid + 1;
+    } 
+
+    low = 0, high = n - 1;
+    int ub = n;    
+    while(low <= high){
+        int mid = (high + low)/2;
+        if(arr[mid] > x){
+            ub = mid;
+            high = mid - 1;
+        }
+        else low = mid + 1;
+    }
+    if(arr[lb] != x) return {-1, -1};
+    return {lb, ub - 1};
+}
+
+
+
+// LB and While Loop Solution
 pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int x)
 {
     // Write your code here
